5a5a8282d84b74404589711a428461c5e728997f
ruwanau <41232464+ruwanau@users.noreply.github.com>
2026-05-24 00:21:51 +1000

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,4 @@
+// Bare minimum service worker to allow home screen installation
+self.addEventListener('fetch', function(event) {
+    // Intentionally left blank to just forward web traffic naturally
+});
